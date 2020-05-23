97eb7e9e68ab6ad6fd0960fb67d0fb8e779e9267
Yoav Levin <yoav.levin@gmail.com>
2020-05-23 09:21:25 +0300

    init

---
+++ src/Form.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import Response from "./Response";
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import "./App.css";
+
+function ValidationForm() {
+  const [state, setState] = useState({
+    email: null,
+    fixTypos: false,
+    info: null,
+    isSubmitted: false,
+  });
+
+  const handleEmailChange = async (e: any) => {
+    await setState({
+      ...state,
+      email: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const params = {
+      email: state.email,
+      fixTypos: state.fixTypos,
+    };
+    const res = await axios.get(
+      "https://csqa-email-validator.herokuapp.com/validate",
+      { params }
+    );
+    setState({ ...state, isSubmitted: true });
+  };
+
+  return (
+    <div className="App-form">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="emailValidation">
+          <Form.Label>Email adress: </Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            className="input-field"
+            onChange={handleEmailChange}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Please tick for validation: </Form.Label>
+          <Form.Check
+            type="checkbox"
+            label="Fix Typos"
+            className="field"
+            onChange={(event: any) =>
+              setState({ ...state, fixTypos: event.target.checked })
+            }
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default ValidationForm;
---
+++ src/Response.tsx
@@ -0,0 +1,25 @@
+import React,{ useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ListGroup from "react-bootstrap/ListGroup";
+
+function Response(props: any) {
+    const [state, setState] = useState({
+        isValid: true,
+      });
+
+  return (
+
+    <div className="App">
+      <ListGroup>
+        <ListGroup.Item variant = {state.isValid ? 'warning': 'info'} >Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
+}
+
+export default Response;

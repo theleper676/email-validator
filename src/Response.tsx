b84f60a6e3d10bd8464e88ed224d7d5a385ee9ca
Yoav Levin <yoav.levin@gmail.com>
2020-05-26 08:32:53 +0300

    added response props

--- src/Response.tsx
+++ src/Response.tsx
@@ -4,15 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ListGroup from "react-bootstrap/ListGroup";
 
 function Response(props: any) {
-    const {email}
-    const [state, setState] = useState({
-      });
-
   return (
 
     <div className="App">
       <ListGroup>
-  <ListGroup.Item variant = {state.isValid ? 'warning': 'info'} >{props.email}</ListGroup.Item>
+  <ListGroup.Item>{props.info.email}</ListGroup.Item>
         <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
         <ListGroup.Item>Morbi leo risus</ListGroup.Item>
         <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>

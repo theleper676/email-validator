c2e4d31cd2cb42cec3dea4364465b5cf980d6f6e
Yoav Levin <yoav.levin@gmail.com>
2020-05-28 10:14:52 +0300

    fed changed

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,8 +1,11 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import Response from './Response';
+import Response from "./Response";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./App.css";
 
 function ValidationForm() {
@@ -13,12 +16,12 @@ function ValidationForm() {
     isSubmitted: false,
   });
 
-  useEffect(()=> {
-    console.log(state);
-  })
+  useEffect(() => {
+    console.log(state.info);
+  });
 
   const handleEmailChange = async (e: any) => {
-    const {value} = e.target;
+    const { value } = e.target;
     await setState({
       ...state,
       email: value,
@@ -31,46 +34,62 @@ function ValidationForm() {
       email: state.email,
       fixTypos: state.fixTypos,
     };
-    try{
+    try {
       const res = await axios.get(
         "https://csqa-email-validator.herokuapp.com/validate",
         { params }
       );
-      setState({...state,info: res.data,isSubmitted: true}); 
-    }
-    catch(err){
+      setState({ ...state, info: res.data, isSubmitted: true });
+    } catch (err) {
       console.log(err);
     }
-    
   };
 
   return (
     <div className="App-form">
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="emailValidation">
-          <Form.Label>Email adress: </Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter Email"
-            className="input-field"
-            onChange={handleEmailChange}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Please tick for validation: </Form.Label>
-          <Form.Check
-            type="checkbox"
-            label="Fix Typos"
-            className="field"
-            onChange={(event: any) =>
-              setState({ ...state, fixTypos: event.target.checked })
-            }
-          />
-          {state.isSubmitted && <Response info={state.info} /> }
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <Container>
+          <Row>
+            <Col>
+            <Form.Group controlId="emailValidation">
+              <Form.Label>Email adress: </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                className="input-field"
+                onChange={handleEmailChange}
+              />
+            </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col> <Form.Group>
+              <Form.Label>Please tick for validation: </Form.Label>
+              <Form.Check
+                type="checkbox"
+                label="Fix Typos"
+                className="field"
+                onChange={(event: any) =>
+                  setState({ ...state, fixTypos: event.target.checked })
+                }
+              />
+              {state.isSubmitted && <Row><Col lg={"auto"}><Response info={state.info}/></Col></Row>}
+            </Form.Group></Col>
+           
+          </Row>
+
+          <Row>
+            <Col>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            </Col>
+           <Col>
+           <Button variant="danger">Clear</Button>
+           </Col>
+           
+          </Row>
+        </Container>
       </Form>
     </div>
   );
--- src/Response.tsx
+++ src/Response.tsx
@@ -1,19 +1,34 @@
-import React,{ useState } from "react";
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Response(props: any) {
+  const { domain, domain_error, email, mx_records_found, valid } = props.info;
   return (
-
-    <div className="App">
-      <ListGroup>
-  <ListGroup.Item>{props.info.email}</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
+    <div className="Response">
+      <Container>
+        <Row>
+          <Col lg={"auto"}>
+          <ListGroup style={{ width: "22rem" }}>
+            <ListGroup.Item><b>Email:</b> {email}</ListGroup.Item>
+            <ListGroup.Item><b>Domain:</b> {domain}</ListGroup.Item>
+            <ListGroup.Item variant={valid ? "success" : "danger"}>
+              <b>Is Email Valid?</b> {valid.toString()}
+            </ListGroup.Item>
+            <ListGroup.Item variant={mx_records_found ? "success" : "danger"}>
+              <b>MX Records found?</b> {mx_records_found.toString()}
+            </ListGroup.Item>
+            <ListGroup.Item variant={domain_error ? "danger" : "success"}>
+             <b>Domain Error?</b>  {domain_error.toString()}
+            </ListGroup.Item>
+          </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }

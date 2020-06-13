4c446bc1a9b206103b7ff8a2cd4f7e33af18911c
Yoav Levin <yoav.levin@gmail.com>
2020-06-13 16:47:24 +0300

    added DNS resolve

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
@@ -7,18 +7,23 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Loader from "./Loader";
-import dotenv from 'dotenv';
-
-
+import dotenv from "dotenv";
 
 function ValidationForm() {
-
   dotenv.config();
 
   interface queryParams {
-    email: string | null,
-    fixTypos?: boolean,
-    api_key?: string,
+    email: string | null;
+    fixTypos?: boolean;
+    api_key?: string;
+  }
+
+  interface DNSrequest {
+    target: string | null;
+    param: string;
+    dnsServer: string;
+    location?: string;
+    limit: number;
   }
   const [state, setState] = useState({
     email: null,
@@ -29,6 +34,13 @@ function ValidationForm() {
     hunterInfo: null,
   });
 
+  const [DNSstate, setDNSstate ] = useState(null);
+
+  useEffect(()=> {
+    console.log(DNSstate)
+  })
+
+
   const handleEmailChange = async (e: any) => {
     const { value } = e.target;
     await setState({
@@ -37,6 +49,8 @@ function ValidationForm() {
     });
   };
 
+
+ 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     setState({
@@ -47,29 +61,49 @@ function ValidationForm() {
     GetHunterInfo();
   };
 
-  function GetHunterInfo () {
+  const getDNSinfo =  () => {
+
+    axios.post('https://api.perfops.net/run/dns-resolve',{
+
+      target: state.email,
+      param: "NS",
+      dnsServer: "127.0.0.1",
+      location: "world",
+      limit: 1,
+    })
+    .then(res => {
+      axios.get(`https://api.perfops.net/run/dns-resolve/${res.data.id}`)
+      .then(DNSres => setDNSstate(DNSres.data));
+    })
+};
+
+  function GetHunterInfo() {
     const API = "https://csqa-email-validator.herokuapp.com/validate";
-    const hunterAPI = 'https://hunter.io/v2/email-verifier';
+    const hunterAPI = "https://hunter.io/v2/email-verifier";
 
     const params: queryParams = {
       email: state.email,
-      api_key: '4c4fdfa41329e980977210df5bddf568d904f8a5',
+      api_key: "4c4fdfa41329e980977210df5bddf568d904f8a5",
     };
 
-    const req1 = axios.get(API,{params});
-    const req2 = axios.get(hunterAPI,{params});
+    const req1 = axios.get(API, { params });
+    const req2 = axios.get(hunterAPI, { params });
 
+    axios.all([req1, req2]).then(
+      axios.spread((...responses: any[]) => {
+        const res1 = responses[0];
+        const res2 = responses[1];
 
-  axios.all([req1,req2])
-  .then(axios.spread((...responses) =>{
-    const res1 = responses[0];
-    const res2= responses[1];
-
-  setState({ ...state, info: res1.data, isSubmitted: true,isLoading: false, hunterInfo: res2.data.data });
-  }));
-
+        setState({
+          ...state,
+          info: res1.data,
+          isSubmitted: true,
+          isLoading: false,
+          hunterInfo: res2.data.data,
+        });
+      })
+    );
   }
-  
 
   return (
     <div className="App-form">
@@ -77,48 +111,53 @@ function ValidationForm() {
         <Container>
           <Row>
             <Col>
-            <Form.Group controlId="emailValidation">
-              <Form.Label>Email adress: </Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter Email"
-                className="input-field"
-                onChange={handleEmailChange}
-              />
-            </Form.Group>
+              <Form.Group controlId="emailValidation">
+                <Form.Label>Email adress: </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter Email"
+                  className="input-field"
+                  onChange={handleEmailChange}
+                />
+              </Form.Group>
             </Col>
-          </Row>
-          <Row>
-            <Col> <Form.Group>
-              <Form.Label>Please tick for validation: </Form.Label>
-              <Form.Check
-                type="checkbox"
-                label="Fix Typos"
-                className="field"
-                onChange={(event: any) =>
-                  setState({ ...state, fixTypos: event.target.checked })
-                }
-              />
-              
-            </Form.Group></Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-            {state.isLoading && <Loader />}
-              {state.isSubmitted && <Response info={state.info} hunterInfo={state.hunterInfo}/>}
-            </Col>
-         
           </Row>
           <Row>
             <Col>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+              {" "}
+              <Form.Group>
+                <Form.Label>Please tick for validation: </Form.Label>
+                <Form.Check
+                  type="checkbox"
+                  label="Fix Typos"
+                  className="field"
+                  onChange={(event: any) =>
+                    setState({ ...state, fixTypos: event.target.checked })
+                  }
+                />
+              </Form.Group>
             </Col>
-           <Col>
-           <Button variant="danger">Clear</Button>
-           </Col>
-           
+          </Row>
+          <Row>
+            <Col xs={12}>
+              {state.isLoading && <Loader />}
+              {state.isSubmitted && (
+                <Response info={state.info} hunterInfo={state.hunterInfo} />
+              )}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Col>
+            <Col>
+              <Button variant="danger">Clear</Button>
+            </Col>
+            <Col>
+              <Button onClick={getDNSinfo} variant={DNSstate ? 'primary' : 'info'}>Get DNS status</Button>
+            </Col>
           </Row>
         </Container>
       </Form>
